15f304409c3c1a6bfc6e86a171217af5494df86d
Fedir <99473719+cyanidium1@users.noreply.github.com>
2025-05-01 16:30:59 +0200

    fix GTM

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -61,6 +61,19 @@ export default async function RootLayout({ children, params }) {
       className={`${raleway.className} ${sansation.className} ${manrope.className}`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PCV4WQKM');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link
           rel="icon"
           type="image/png"
@@ -81,6 +94,16 @@ export default async function RootLayout({ children, params }) {
         <link rel="icon" href="/icons/favicon.ico" />
       </head>
       <body className="bg-background text-textColorWhite">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PCV4WQKM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider messages={messages}>
           <Header />
           {children}
